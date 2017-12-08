2058e8d39f1b463792368ef086227bc3cdc0fd70
Melanie MacDonald <melmacd94@gmail.com>
2017-12-08 15:31:21 -0500

    woooooo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require('method-override');
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
