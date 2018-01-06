b5df1c9bc34ff89fb0beaa645210251c3cac05cc
Melanie MacDonald <melmacd94@gmail.com>
2018-01-06 15:45:06 -0500

    git fix

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-  id int NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(250) NOT NULL,
   devoured BOOLEAN DEFAULT false,
-  date TIMESTAMP,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY(id)
 );
